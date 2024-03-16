3cd7256827bcc28727287ef76b2b5027b77cd87d
unknown <laiquan163@gmail.com>
2024-03-16 22:41:47 +0700

    2nd commit 16.3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,56 @@
+// //api
+let prjBlock = document.querySelector('.top-cnt-main');
+
+function getAndShowProjects() {
+    let apiURL = 'https://ems.ioteamvn.com//test/test1?json={"token":"O1L486UPS9MVY7jcihhe4idshRBb0TyD"}';
+    let data = fetch(apiURL).then(res => res.json())
+    console.log(data)
+    fetch(apiURL)
+        .then(res => res.json())
+        .then(dataObject => {
+            let parentCount = Object.keys(dataObject).length;
+            console.log(parentCount);
+
+            for (const key in dataObject) {
+                if (dataObject.hasOwnProperty(key)) {
+                    const projectData = dataObject[key];
+                    const projectDiv = document.createElement("div");
+                    projectDiv.classList.add("prj-view");
+
+                    const eyeWrapper = document.createElement("div");
+                    eyeWrapper.classList.add("i-wrapper");
+                    const eyeIcon = document.createElement("i");
+                    eyeIcon.classList.add("fa-solid", "fa-eye");
+                    eyeWrapper.appendChild(eyeIcon);
+                    projectDiv.appendChild(eyeWrapper);
+
+                    const prjMainDiv = document.createElement("div");
+                    prjMainDiv.classList.add("prj-main");
+                    const prjNameSpan = document.createElement("span");
+                    prjNameSpan.classList.add("prj-cnt");
+                    prjNameSpan.textContent = key;
+
+                    const onlineCount = projectData.filter(item => item[1] === "Online").length;
+                    const totalCount = projectData.length;
+
+                    const prjInfoSpan = document.createElement("span");
+                    prjInfoSpan.classList.add("prj-info");
+                    prjInfoSpan.textContent = `On: ${onlineCount}/${totalCount}`;
+
+                    prjMainDiv.appendChild(prjNameSpan);
+                    prjMainDiv.appendChild(prjInfoSpan);
+                    projectDiv.appendChild(prjMainDiv);
+
+                    prjBlock.appendChild(projectDiv);
+                }
+            }
+        })
+        .catch(error => {
+            console.error('Lỗi:', error);
+        });
+}
+getAndShowProjects();
+
 // create chart
 const ctx = document.getElementById('myChart');
 new Chart(ctx, {
@@ -27,24 +80,33 @@ chartmnbtn.addEventListener('click', function () {
     if (chartmenu.classList.contains('hide')) {
         chartmenu.classList.add('show');
         chartmenu.classList.remove('hide');
-    }else {
+    } else {
         chartmenu.classList.remove('show');
         chartmenu.classList.add('hide');
     }
 })
 
 //view project
+const test = document.querySelector('.top-cnt-main')
 const projectlist = document.querySelectorAll('.prj-view');
 const projectDis = document.querySelector('.prj-chart-js');
 
-for (const projectBtn of projectlist){
-    projectBtn.addEventListener('click', function() {
-        if (projectDis.classList.contains('hide')){
+for (const projectBtn of projectlist) {
+    test.addEventListener('click', function (e) {
+        if (projectDis.classList.contains('hide')) {
             projectDis.classList.add('show');
             projectDis.classList.remove('hide');
-        }else {
+        } else {
             projectDis.classList.add('hide');
             projectDis.classList.remove('show');
         }
     })
 }
+
+
+
+
+
+
+
+
