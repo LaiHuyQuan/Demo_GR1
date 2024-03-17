cea4106a1ff7affda7b1fb206b7eb4522b44e8cc
unknown <laiquan163@gmail.com>
2024-03-17 20:39:48 +0700

    re update 17.3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,142 +1,84 @@
-//api
-let prjBlock = document.querySelector('.top-cnt-main');
+$(document).ready(function() {
+    function addProject(project, online, total){
+        var htmlString = '<div class="col-6 col-lg-3 col-md-6 prjBtn">' +
+                            '<div class="card">' +
+                                '<div class="card-body px-4 py-4-5">' +
+                                    '<div class="row">' +
+                                        '<div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">' +
+                                            '<div class="stats-icon purple mb-2">' +
+                                                '<i class="fa-solid fa-eye fa-xs"></i>' +
+                                            '</div>' +
+                                        '</div>' +
+                                        '<div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">' +
+                                            '<h6 class="text-muted font-semibold">' + project +'</h6>' +
+                                            '<h6 class="font-extrabold mb-0">On:'+ online +'/'+ total+'</h6>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>';
+    
+        $('.prjList').append(htmlString);
+    }
 
-let apiURL = 'https://ems.ioteamvn.com//test/test1?json={"token":"O1L486UPS9MVY7jcihhe4idshRBb0TyD"}';
-let data = fetch(apiURL).then(res => res.json())
-console.log(data)
-fetch(apiURL)
-    .then(res => res.json())
-    .then(dataObject => {
-        let parentCount = Object.keys(dataObject).length;
-        console.log(parentCount);
-
-        let htmlContent = '';
-        for (const key in dataObject) {
-            if (dataObject.hasOwnProperty(key)) {
-                const projectData = dataObject[key];
-
-                const onlineCount = projectData.filter(item => item[1] === "Online").length;
-                const totalCount = projectData.length;
-
-                htmlContent += `<div class="prj-view prj-view-js" data-project='${JSON.stringify(projectData)}'>
-                                        <div class="i-wrapper prj-view-js">
-                                            <i class="fa-solid fa-eye prj-view-js"></i>
-                                        </div>
-                                        <div class="prj-main prj-view-js">
-                                            <span class="prj-cnt prj-view-js">${key}</span>
-                                            <span class="prj-info prj-view-js">Online: ${onlineCount}/${totalCount}</span>
-                                        </div>
-                                    </div>`;
-            }
+    $.ajax({
+        url: 'https://ems.ioteamvn.com//test/test1',
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            json: '{"token":"O1L486UPS9MVY7jcihhe4idshRBb0TyD"}'
+        },
+        success: function(data) {
+            console.log(data); 
+            var result = countOnlineElements(data);
+            console.log(result);
+        },
+        error: function(xhr, status, error) {
+            // Xử lý lỗi ở đây
+            console.error(status, error);
         }
-        prjBlock.innerHTML = htmlContent;
-    })
-    .catch(error => {
-        console.error('Lỗi:', error);
     });
 
+    function countOnlineElements(data) {
+        var counts = {};
+    
+        $.each(data, function(project, projectData) {
+            var onlineCount = 0;
+            var totalCount = projectData.length;
+    
+            $.each(projectData, function(index, item) {
+                if (item[1] === 'Online') {
+                    onlineCount++;
+                }
+            });
+    
+            counts[project] = {
+                online: onlineCount,
+                total: totalCount
+            };
+            addProject(project, onlineCount, totalCount);
+        });
+    
+        return counts;
+    };
+    
+    $('.prjList').on('click', '.prjBtn', function() {
+        alert('Button clicked!');
+    });
 
-// // create chart
-// const ctx = document.getElementById('myChart');
-// new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Jan', 'Feb', 'Mar'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3, 6, 1, 33],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             y: {
-//                 beginAtZero: true
-//             }
-//         }
-//     }
-// });
-
-// // chart menu
-// const chartmnbtn = document.querySelector('.chart-menu-js');
-// const chartmenu = document.querySelector('.chart-menu');
-
-// chartmnbtn.addEventListener('click', function () {
-//     if (chartmenu.classList.contains('hide')) {
-//         chartmenu.classList.add('show');
-//         chartmenu.classList.remove('hide');
-//     } else {
-//         chartmenu.classList.remove('show');
-//         chartmenu.classList.add('hide');
-//     }
-// })
-
-// view project
-const test = document.querySelector('.top-cnt-main');
-const projectDis = document.querySelector('.prj-chart-js')
-let currentTable = null;
-
-test.addEventListener('click', function (e) {
-    if (e.target.matches('.prj-view-js')) {
-        const projectData = JSON.parse(e.target.getAttribute('data-project'));
-        console.log(projectData);
-
-        let tableHTML = `
-            <div class="prj-chart-hd">
-                <span>Project Visit</span>
-                <i class="fa-solid fa-x"></i>
-            </div>
-            <table class="project-table" cellpadding="5" cellspacing="0" width="50%" align="left">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>STATUS</th>
-                    </tr>
-                </thead>
-                <tbody>
-        `;
-
-        for (let i = 0; i < projectData.length; i++) {
-            const rowData = projectData[i];
-            if(rowData[1] ==  'Online'){
-                tableHTML += `
-                    <tr>
-                        <td>${rowData[0]}</td>
-                        <td >
-                            <span class='online'>${rowData[1]}</span>
-                        </td>
-                    </tr>
-                `;
-            }else{
-                tableHTML += `
-                    <tr>
-                        <td>${rowData[0]}</td>
-                        <td class='offline'>${rowData[1]}</td>
-                    </tr>
-                `;
-            }
-        }
-
-        tableHTML += `
-                </tbody>
-            </table>
-        `;
-
-        projectDis.innerHTML = tableHTML;
-
-        if (currentTable) {
-            currentTable.classList.add('hide');
-        }
-        projectDis.classList.remove('hide');
-        projectDis.classList.add('show');
-        currentTable = projectDis;
+    function createTableRowHTML(id, status) {
+        var htmlString = '<tr>' +
+                            '<td class="text-bold-500">' + id + '</td>' +
+                            '<td>' + status + '</td>' +
+                        '</tr>';
+        return htmlString;
     }
-});
-
-projectDis.addEventListener('click', function (e) {
-    if(e.target.matches('.fa-x')){
-        projectDis.classList.add('hide');
-        projectDis.classList.remove('show');
-    }
+    
+    var idValue = 'Michael Right';
+    var statusValue = '$15/hr';
+    var newTableRowHTML = createTableRowHTML(idValue, statusValue);
+    
+    $('.table tbody').append(newTableRowHTML);
+    
 });
 
