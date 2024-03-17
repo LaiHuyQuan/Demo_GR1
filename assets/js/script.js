297bb8a0c11e9515eb546e5daab0fb774be7aed7
unknown <laiquan163@gmail.com>
2024-03-17 09:28:37 +0700

    1st update 17.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,92 @@
 // //api
-let prjBlock = document.querySelector('.top-cnt-main');
+// let prjBlock = document.querySelector('.top-cnt-main');
 
-function getAndShowProjects() {
-    let apiURL = 'https://ems.ioteamvn.com//test/test1?json={"token":"O1L486UPS9MVY7jcihhe4idshRBb0TyD"}';
-    let data = fetch(apiURL).then(res => res.json())
-    console.log(data)
-    fetch(apiURL)
-        .then(res => res.json())
-        .then(dataObject => {
-            let parentCount = Object.keys(dataObject).length;
-            console.log(parentCount);
+// function getAndShowProjects() {
+//     let apiURL = 'https://ems.ioteamvn.com//test/test1?json={"token":"O1L486UPS9MVY7jcihhe4idshRBb0TyD"}';
+//     let data = fetch(apiURL).then(res => res.json())
+//     console.log(data)
+//     fetch(apiURL)
+//         .then(res => res.json())
+//         .then(dataObject => {
+//             let parentCount = Object.keys(dataObject).length;
+//             console.log(parentCount);
 
-            for (const key in dataObject) {
-                if (dataObject.hasOwnProperty(key)) {
-                    const projectData = dataObject[key];
-                    const projectDiv = document.createElement("div");
-                    projectDiv.classList.add("prj-view");
+//             let htmlContent = '';
+//             for (const key in dataObject) {
+//                 if (dataObject.hasOwnProperty(key)) {
+//                     const projectData = dataObject[key];
 
-                    const eyeWrapper = document.createElement("div");
-                    eyeWrapper.classList.add("i-wrapper");
-                    const eyeIcon = document.createElement("i");
-                    eyeIcon.classList.add("fa-solid", "fa-eye");
-                    eyeWrapper.appendChild(eyeIcon);
-                    projectDiv.appendChild(eyeWrapper);
+//                     const onlineCount = projectData.filter(item => item[1] === "Online").length;
+//                     const totalCount = projectData.length;
 
-                    const prjMainDiv = document.createElement("div");
-                    prjMainDiv.classList.add("prj-main");
-                    const prjNameSpan = document.createElement("span");
-                    prjNameSpan.classList.add("prj-cnt");
-                    prjNameSpan.textContent = key;
-
-                    const onlineCount = projectData.filter(item => item[1] === "Online").length;
-                    const totalCount = projectData.length;
-
-                    const prjInfoSpan = document.createElement("span");
-                    prjInfoSpan.classList.add("prj-info");
-                    prjInfoSpan.textContent = `On: ${onlineCount}/${totalCount}`;
-
-                    prjMainDiv.appendChild(prjNameSpan);
-                    prjMainDiv.appendChild(prjInfoSpan);
-                    projectDiv.appendChild(prjMainDiv);
-
-                    prjBlock.appendChild(projectDiv);
-                }
-            }
-        })
-        .catch(error => {
-            console.error('Lỗi:', error);
-        });
-}
-getAndShowProjects();
-
-// create chart
-const ctx = document.getElementById('myChart');
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Jan', 'Feb', 'Mar'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3, 6, 1, 33],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
-
-// chart menu
-const chartmnbtn = document.querySelector('.chart-menu-js');
-const chartmenu = document.querySelector('.chart-menu');
-
-chartmnbtn.addEventListener('click', function () {
-    if (chartmenu.classList.contains('hide')) {
-        chartmenu.classList.add('show');
-        chartmenu.classList.remove('hide');
-    } else {
-        chartmenu.classList.remove('show');
-        chartmenu.classList.add('hide');
-    }
-})
-
-//view project
-const test = document.querySelector('.top-cnt-main')
-const projectlist = document.querySelectorAll('.prj-view');
-const projectDis = document.querySelector('.prj-chart-js');
-
-for (const projectBtn of projectlist) {
-    test.addEventListener('click', function (e) {
-        if (projectDis.classList.contains('hide')) {
-            projectDis.classList.add('show');
-            projectDis.classList.remove('hide');
-        } else {
-            projectDis.classList.add('hide');
-            projectDis.classList.remove('show');
-        }
-    })
-}
+//                     htmlContent += `<div class="prj-view prj-view-js">
+//                                         <div class="i-wrapper prj-view-js">
+//                                             <i class="fa-solid fa-eye prj-view-js"></i>
+//                                         </div>
+//                                         <div class="prj-main prj-view-js">
+//                                             <span class="prj-cnt prj-view-js">${key}</span>
+//                                             <span class="prj-info prj-view-js">On: ${onlineCount}/${totalCount}</span>
+//                                         </div>
+//                                     </div>`;
+//                 }
+//             }
+//             prjBlock.innerHTML = htmlContent;
+//         })
+//         .catch(error => {
+//             console.error('Lỗi:', error);
+//         });
+// }
+// getAndShowProjects();
 
 
+// // create chart
+// const ctx = document.getElementById('myChart');
+// new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Jan', 'Feb', 'Mar'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3, 6, 1, 33],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
 
+// // chart menu
+// const chartmnbtn = document.querySelector('.chart-menu-js');
+// const chartmenu = document.querySelector('.chart-menu');
 
+// chartmnbtn.addEventListener('click', function () {
+//     if (chartmenu.classList.contains('hide')) {
+//         chartmenu.classList.add('show');
+//         chartmenu.classList.remove('hide');
+//     } else {
+//         chartmenu.classList.remove('show');
+//         chartmenu.classList.add('hide');
+//     }
+// })
 
+// //view project
+// const test = document.querySelector('.top-cnt-main');
+// const projectDis = document.querySelector('.prj-chart-js');
+// const projectlist = document.querySelectorAll('.prj-view-js');
 
-
-
+// test.addEventListener('click', function (e) {
+//     if (e.target && e.target.matches('.prj-view-js')) {
+//         if (projectDis.classList.contains('hide')) {
+//             projectDis.classList.add('show');
+//             projectDis.classList.remove('hide');
+//         } else {
+//             projectDis.classList.add('hide');
+//             projectDis.classList.remove('show');
+//         }
+//     }
+// });
