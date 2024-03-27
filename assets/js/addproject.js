338c366c36eb367ec242f8a42b86aa5ee3f7a89c
unknown <laiquan163@gmail.com>
2024-03-27 23:29:32 +0700

    update 27.3 structure

--- assets/js/addproject.js
+++ assets/js/addproject.js
@@ -7,9 +7,21 @@ var projectData = {
 
 // structure dự án
 var jsonStructure = {
-    "level1": "nhà 1",
-    "level1Child": []
+    "level0": {
+        "level1" :{
+            "name" : "1233",
+            "level2" :{
+                "name":"",
+                "level3":{
+                    "name":"",
+                    "level4":[]
+                }
+            }       
+        }
+    }
 };
+
+var jsonData;
 
 
 $(document).ready(function () {
@@ -327,15 +339,108 @@ $(document).ready(function () {
         return true;
     }
 
-    // test
-    $('.add-button').click(function(){
-        var level = $(this).data('level');
-        var newBlock = $('<div class="block block-level-' + level + '">Cấp Độ ' + level + '</div>');
-        $(this).parent().append(newBlock); // Changed this line to use parent()
-    });
-    $('.block').click(function(event){
-        event.stopPropagation();
-        $(this).children('.block-level-' + (parseInt($(this).attr('class').match(/block-level-(\d+)/)[1]) + 1)).slideToggle();
+    // Structure
+    var isAdding = false; // Biến để kiểm tra trạng thái đang thêm mới hay không
+
+    $('.container').on('click', '.add-button', function () {
+        var parentBlock = $(this).parent(); // Lưu trữ khối cha
+        if (parentBlock.find('.add-input').length) {
+            parentBlock.find('.add-input').remove(); // Xóa input hiện có nếu đang tồn tại trong khối cha
+            parentBlock.find('.cancel-button').remove(); // Xóa nút hủy trong khối cha
+        }
+        isAdding = true; // Thiết lập trạng thái đang thêm mới
+        var level = parseInt(parentBlock.attr('class').match(/block-level-(\d+)/)[1]) + 1;
+        parentBlock.append('<input type="text" class="add-input" placeholder="Nhập tên cho Cấp Độ ' + level + '">');
+        parentBlock.append('<button class="cancel-button">Hủy</button>'); // Thêm nút hủy vào khối cha
+        parentBlock.children('.block-level-' + (level - 1) + ' > .block').each(function () {
+            if (!$(this).is(':visible')) {
+                $(this).slideToggle(); // Nếu không, hiện chúng ra
+            }
+        });
+        $('.add-input').focus(); // Focus vào input mới
     });
 
+
+    $('.container').on('click', '.cancel-button', function (event) {
+        event.stopPropagation(); // Ngăn chặn sự kiện click lan truyền tới các khối cha
+        $('.add-input').remove(); // Xóa input
+        $(this).remove(); // Xóa nút hủy
+        isAdding = false; // Trở lại trạng thái không thêm mới
+    });
+
+    $('.container').on('click', '.delete-button', function (event) {
+        event.stopPropagation(); // Ngăn chặn sự kiện click lan truyền tới các khối cha
+        $(this).parent().remove(); // Xóa cấp độ hiện tại khi nút xóa được nhấn
+    });
+
+    $('.container').on('keydown', '.add-input', function (event) {
+        event.stopPropagation(); // Ngăn chặn sự kiện click từ nút thêm input truyền vào
+        if (event.which === 13) { // Kiểm tra nếu phím Enter được nhấn
+            var inputText = $(this).val();
+            var level = parseInt($(this).parent().attr('class').match(/block-level-(\d+)/)[1]);
+            var newBlock;
+            if (level < 3) {
+                newBlock = '<div class="block block-level-' + (level + 1) + '">' + inputText  +
+                '<button class="add-button">+</button>' +
+                '<button class="delete-button">-</button>' +
+                '</div>';
+            }else{
+                newBlock = '<div class="block block-level-' + (level + 1) + '">' + inputText  +
+                '<button class="delete-button">-</button>' +
+                '</div>';
+            }
+
+            $(this).parent().append(newBlock);
+            $(this).remove(); // Xóa input sau khi thêm cấp độ mới
+            $('.cancel-button').remove(); // Xóa nút hủy
+            isAdding = false; // Trở lại trạng thái không thêm mới
+        }
+    });
+
+    $('.container').on('click', '.block', function () {
+        if (!isAdding) {
+            $(this).children('.block-level-2').slideToggle();
+        }
+    });
+
+    $('.container').on('click', '.block-level-2', function (event) {
+        event.stopPropagation();
+        if (!isAdding) {
+            $(this).children('.block-level-3').slideToggle();
+        }
+    });
+
+    $('.container').on('click', '.block-level-3', function (event) {
+        event.stopPropagation();
+        if (!isAdding) {
+            $(this).children('.block-level-4').slideToggle();
+        }
+    });
+
+    $('.container').on('click', '.block-level-4', function (event) {
+        event.stopPropagation();
+    });
+    // test
+    $(document).ready(function() {
+        var jsonData = readData($('.container')); // Đọc dữ liệu từ container và lưu vào biến jsonData
+        console.log(jsonData); // In ra dữ liệu đã lưu
+        
+        function readData($element) {
+            var block = {}; // Đối tượng để lưu dữ liệu
+            block.name = $element.contents().filter(function() {
+                return this.nodeType === 3; // Chỉ lấy text nodes
+            }).text().trim(); // Lưu tên của cấp độ
+    
+            block.children = []; // Mảng để lưu các cấp độ con
+            
+            // Lặp qua các cấp độ con của phần tử hiện tại
+            $element.children('.block').each(function() {
+                var $childElement = $(this);
+                var childBlock = readData($childElement); // Đọc dữ liệu của cấp độ con
+                block.children.push(childBlock); // Thêm cấp độ con vào mảng
+            });
+    
+            return block;
+        }
+    });
 });
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,38 +1,48 @@
 $(document).ready(function(){
     var isAdding = false; // Biến để kiểm tra trạng thái đang thêm mới hay không
 
-    $('.block').on('click', '.add-button', function() {
-        if ($('.add-input').length) {
-            $('.add-input').remove(); // Xóa input hiện có nếu đang tồn tại
-            $('.cancel-button').remove(); // Ẩn nút hủy
+    $('.container').on('click', '.add-button', function() {
+        var parentBlock = $(this).parent(); // Lưu trữ khối cha
+        if (parentBlock.find('.add-input').length) {
+            parentBlock.find('.add-input').remove(); // Xóa input hiện có nếu đang tồn tại trong khối cha
+            parentBlock.find('.cancel-button').remove(); // Xóa nút hủy trong khối cha
         }
         isAdding = true; // Thiết lập trạng thái đang thêm mới
-        var level = parseInt($(this).parent().attr('class').match(/block-level-(\d+)/)[1]) + 1;
-        $(this).parent().append('<input type="text" class="add-input" placeholder="Nhập tên cho Cấp Độ ' + level + '">');
-        $(this).parent().append('<span class="cancel-button">Hủy</span>'); // Thêm nút hủy
+        var level = parseInt(parentBlock.attr('class').match(/block-level-(\d+)/)[1]) + 1;
+        parentBlock.append('<input type="text" class="add-input" placeholder="Nhập tên cho Cấp Độ ' + level + '">');
+        parentBlock.append('<span class="cancel-button">Hủy</span>'); // Thêm nút hủy vào khối cha
+        parentBlock.children('.block-level-' + (level-1) + ' > .block').each(function() {
+            if (!$(this).is(':visible')) {
+                $(this).slideToggle(); // Nếu không, hiện chúng ra
+            }
+        });
         $('.add-input').focus(); // Focus vào input mới
     });
+    
 
-    $('.block').on('click', '.cancel-button', function(event) {
+    $('.container').on('click', '.cancel-button', function(event) {
         event.stopPropagation(); // Ngăn chặn sự kiện click lan truyền tới các khối cha
         $('.add-input').remove(); // Xóa input
         $(this).remove(); // Xóa nút hủy
         isAdding = false; // Trở lại trạng thái không thêm mới
     });
 
-    $('.block').on('click', '.delete-button', function(event) {
+    $('.container').on('click', '.delete-button', function(event) {
         event.stopPropagation(); // Ngăn chặn sự kiện click lan truyền tới các khối cha
         $(this).parent().remove(); // Xóa cấp độ hiện tại khi nút xóa được nhấn
     });
 
-    $('.block').on('keydown', '.add-input', function(event) {
+    $('.container').on('keydown', '.add-input', function(event) {
+        event.stopPropagation(); // Ngăn chặn sự kiện click từ nút thêm input truyền vào
         if (event.which === 13) { // Kiểm tra nếu phím Enter được nhấn
             var inputText = $(this).val();
             var level = parseInt($(this).parent().attr('class').match(/block-level-(\d+)/)[1]);
             var newBlock = $('<div class="block block-level-' + (level + 1) + '">' + inputText + '</div>');
+
             if(level < 3){
                 newBlock.append('<span class="add-button">+</span>');
             }
+            
             newBlock.append('<span class="delete-button">-</span>');
             $(this).parent().append(newBlock);
             $(this).remove(); // Xóa input sau khi thêm cấp độ mới
@@ -41,31 +51,27 @@ $(document).ready(function(){
         }
     });
 
-    $('.block').click(function(){
+    $('.container').on('click', '.block', function() {
         if (!isAdding) {
             $(this).children('.block-level-2').slideToggle();
         }
     });
 
-    $('.block-level-2').click(function(event){
+    $('.container').on('click', '.block-level-2', function(event) {
         event.stopPropagation();
         if (!isAdding) {
             $(this).children('.block-level-3').slideToggle();
         }
     });
 
-    $('.block-level-3').click(function(event){
+    $('.container').on('click', '.block-level-3', function(event) {
         event.stopPropagation();
         if (!isAdding) {
             $(this).children('.block-level-4').slideToggle();
         }
     });
 
-    $('.block-level-4').click(function(event){
+    $('.container').on('click', '.block-level-4', function(event) {
         event.stopPropagation();
-    });
-
-    $('.block').on('click', '.add-input', function(event) {
-        event.stopPropagation(); // Ngăn chặn sự kiện click từ nút thêm input truyền vào
     });
 });
